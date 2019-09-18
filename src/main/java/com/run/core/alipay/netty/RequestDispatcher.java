4e18dfc87ce21148dc30cb306ddb5d25e91f84be
AleeX007 <aleex007@163.com>
2019-09-18 23:25:30 +0800

    消息分发处理开发完毕

--- src/main/java/com/run/core/alipay/netty/RequestDispatcher.java
+++ src/main/java/com/run/core/alipay/netty/RequestDispatcher.java
@@ -1,13 +1,16 @@
 package com.run.core.alipay.netty;
 
+import com.run.core.alipay.service.impl.AliPayService;
 import com.run.core.alipay.utils.ByteDataBuffer;
+import com.run.core.alipay.utils.Cmd;
 import com.run.core.alipay.utils.CommonUtil;
-import io.netty.buffer.ByteBuf;
+import com.run.core.alipay.utils.Compose;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -23,6 +26,9 @@ public class RequestDispatcher implements ApplicationContextAware {
     @Value("${netty.max-threads}")
     private int maxThreads;
 
+    @Autowired
+    private AliPayService aliPayService;
+
     private ExecutorService executorService = Executors.newFixedThreadPool(1024);
     private ApplicationContext app;
 
@@ -30,13 +36,47 @@ public class RequestDispatcher implements ApplicationContextAware {
         executorService.submit(() -> {
             ChannelFuture f = null;
             try {
+                log.info("服务器接收到报文：{}", msg.toString());
                 ByteDataBuffer bdf = new ByteDataBuffer(msg.toString().getBytes("UTF-8"));
+
+                bdf.setInBigEndian(false);
+                int begin = bdf.readInt8();
+                if(begin != 104) {
+                    log.info("---------------------------------------------------------------------");
+                    log.info("返回报文的开始字节格式有误");
+                    log.info("---------------------------------------------------------------------");
+                }
+
                 byte[] lenBytes = new byte[4];
                 bdf.readBytes(lenBytes);
                 String serverCode = bdf.readString(6);
-                // String retData = bdf.readString(CommonUtil.readInt32(lenBytes));
-                log.info("服务器接收到数据：{}", msg.toString());
-                f = ctx.writeAndFlush(msg);
+                String messageFrame = "";
+                String retData = bdf.readString(CommonUtil.readInt32(lenBytes));
+
+                log.info("---------------------------------------------------------------------");
+                log.info("接收到的报文:" + retData);
+                log.info("---------------------------------------------------------------------");
+
+                if(Integer.valueOf(serverCode) == Cmd.CMD_ZFB_SEARCH)
+                    messageFrame = aliPayService.service200001(retData);
+                if(Integer.valueOf(serverCode) == Cmd.CMD_ZFB_PAY)
+                    messageFrame = aliPayService.service200002(retData);
+                if(Integer.valueOf(serverCode) == Cmd.CMD_ZFB_ACCOUNT)
+                    messageFrame = aliPayService.service200012(retData);
+                if(Integer.valueOf(serverCode) == Cmd.CMD_ZFB_STATEMENT)
+                    messageFrame = aliPayService.service200011(retData);
+
+                ByteDataBuffer bdb = new ByteDataBuffer();
+                bdb.setInBigEndian(false);
+                bdb.writeInt8((byte) 0x68); // 开始字节
+                int len = Integer.parseInt(Compose.getLen(messageFrame, 4));
+                // 用于计算数据域的长度是否大于4字节
+                bdb.writeInt32(len); // 报文长度
+                bdb.writeString(serverCode, 6);// 服务编码
+                bdb.writeBytes(messageFrame.getBytes()); // 报文frame
+                bdb.writeInt8((byte) 0x16); // 结束字节
+
+                f = ctx.writeAndFlush(bdb.getBytes());
                 f.addListener(ChannelFutureListener.CLOSE);
             } catch (Exception e) {
                 e.printStackTrace();
---
+++ src/main/java/com/run/core/alipay/utils/Cmd.java
@@ -0,0 +1,14 @@
+package com.run.core.alipay.utils;
+
+public class Cmd {
+
+	// 支付宝查询
+	public final static int CMD_ZFB_SEARCH = 200001;
+	// 支付宝缴费
+	public final static int CMD_ZFB_PAY = 200002;
+	// 支付宝补销账
+	public final static int CMD_ZFB_ACCOUNT = 200012;
+	// 支付宝对账
+	public final static int CMD_ZFB_STATEMENT = 200011;
+
+}
